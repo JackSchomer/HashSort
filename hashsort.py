dcfca4c0b8458606d47a7d0f00d76adb2ffddef6
JackSchomer <54218155+JackSchomer@users.noreply.github.com>
2021-04-02 11:27:29 -0700

    Update hashsort.py
    Change list to array, improve readability

--- hashsort.py
+++ hashsort.py
@@ -1,4 +1,5 @@
 import random
+import array
 from time import perf_counter
 
 def HashSort(array_to_sort):
@@ -6,7 +7,7 @@ def HashSort(array_to_sort):
 	Start=min(array_to_sort)
 	End=max(array_to_sort)
 
-	allocate=[False]*(End-Start)
+	allocate=array.array("b",[False]*(End-Start))
 
 	#O(N)
 	for i in range(len(array_to_sort)):
@@ -22,17 +23,46 @@ def HashedVals(hashsortarray, currentval):
 		currentval+=1
 	return currentval+1
 
-sort1=[]
-sort2=[]
-count=0
-length=10000000
-for i in range(length):
-	sort1.append(i)
-	#sort1.append(2*i)
-	sort2.append(i)
-	#sort2.append(2*i)
-	#sort2_tab.append(False)
+if __name__=='__main__':
 
+	"""Baselining performance:
+	"""
+	sort1=[]
+	sort2=[]
+	count=0
+	length=10000000
+	for i in range(length):
+		sort1.append(2*i)
+		#sort1.append(2*i)
+		sort2.append(2*i)
+		#sort2.append(2*i)
+		#sort2_tab.append(False)
+
+	random.shuffle(sort1)
+	random.shuffle(sort2)
+
+	t1_start=perf_counter()
+
+	sort1.sort()
+
+	t1_stop=perf_counter()
+	#print('elapsed time: ',t1_stop, t1_start)
+	print('built-in sort: ',t1_stop-t1_start)
+
+	t2_start=perf_counter()
+
+	start, hashedsort=HashSort(sort2)
+
+	t2_stop=perf_counter()
+
+	print('hashed sort: ',t2_stop-t2_start)
+
+	currentval = HashedVals(hashedsort, start)
+	print('second val: ', currentval)
+
+	for i in range(10):
+		currentval=HashedVals(hashedsort, currentval)
+		print(currentval)
 random.shuffle(sort1)
 random.shuffle(sort2)
 
